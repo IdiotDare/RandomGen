111fb5051c1356e6284f1336cc852d7d2df29cde
IdiotDare <89470696+IdiotDare@users.noreply.github.com>
2021-12-11 15:20:21 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,5 +5,7 @@ var algChapters=[]
 algTC(subject){
   let theLength = subject.length
   let randNum = Math.floor(Math.random*(theLength-1))
+  if(document.getElementById('algtext').innerHTML != "help"){
   document.getElementById('algtext').innerHTML = "help";
+  }
 }
